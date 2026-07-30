c5a2184162e75b5d9efd403eba421888245d4bb2
anuma8080-bit <anuma8080@gmail.com>
2026-07-30 18:12:07 +0900

    2번문제 완료

---
+++ 02.py
@@ -0,0 +1,11 @@
+c,max = map(int, input().split())
+arr = list(map(int, input().split()))
+total = 0
+discount = 0
+for i in arr:
+	total += i
+
+if total>max:
+	discount = total-max
+
+print(max, discount)
